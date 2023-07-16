f7d67a8f15f35bd3bbe2204c924b818ea82a3248
rupesh68 <rupeshmahajan516@gmail.com>
2023-07-17 01:01:34 +0530

    bg change

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ onValue(shoppingListInDB, function(snapshot) {
             appendItemToShoppingListEl(currentItem)
         }    
     } else {
-        shoppingListEl.innerHTML = "No items here... yet"
+        shoppingListEl.innerHTML = "Note: Double click on item to remove item"
     }
 })
 
